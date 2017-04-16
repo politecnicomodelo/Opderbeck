f2b0bc4a286cb04c2234b49cce33bb9221ca278c
sebastianOP <opderbeck.ipm>
2017-04-16 16:45:18 -0300

    16-4

--- Clases/Empresa_Clases.py
+++ Clases/Empresa_Clases.py
@@ -2,7 +2,13 @@ class Empleado (object):
     nombre = ""
     apellido = ""
     telefono = 0
-    asistencia = []
+    asistencia_mes_dia = [[],[]]
+    mes = ""
+    dia = 0
+
+
+    def __init__(self):
+        asistencia_dia = []
 
     def setNombre (self, nombre):
         self.nombre = str(nombre)
@@ -13,15 +19,34 @@ class Empleado (object):
     def setTelefono (self, telefono):
         self.telefono = str(telefono)
 
-    def setAsitencia (self, asistencia):
-        self.asistencia = str(asistencia)
+    def setAsistenciaDia (self,mes,dia):
+        for item in range(mes):
+            for item2 in range(dia):
+                self.asistencia_mes_dia.append(mes, dia)
+
 
 
 class Empresa (object):
-
+    asistencia_dias = 0
+    asistencia_total = 0
     def __init__(self):
         lista_empleados = []
 
     def Ingresar_Empleado (self, empleado):
         self.lista_empleados.append(empleado)
 
+    def existeEmpleado (self, nombre):
+        for self.lista_empleados in range:
+            if self.nombre == self.lista_empledos:
+                return True
+        return False
+
+    def diasTrabajados (self, mes):
+        for item in range:
+            for item2 in range:
+                if asistencia_mes_dia[item][none]== mes:
+                    self.Asistencia_dias += asistencia_mes_dia[none][item2]
+                    for item3 in range (self.asistencia_dias):
+                        if self.asistencia_dias[item] == 1:
+                            asistencia_total += 1
+        return asistencia_total
--- Empresa_Principal.py
+++ Empresa_Principal.py
@@ -1,2 +1,30 @@
+#:::::::::::::::::CODIGOS DE MESES::::::::::::::::::::::::::::::
+#E...enero          MY...mayo       S...septiembre
+#F...febrero        JN...junio      O...octubre
+#MZ...marzo         JL...julio      N...noviembre
+#AB...abril         AG...agosto     D...diciembre
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+#IMPORTANDO CLASES:::::::::::::::::::::::::::::::::::::::::::::
 from Clases.Empresa_Clases import *
 
+
+#CREANDO EMPLEADO::::::::::::::::::::::::::::::::::::::::::::::
+empleado1= Empleado
+
+
+#ASIGNANDO DATOS AL EMPLEADO:::::::::::::::::::::::::::::::::::
+empleado1.nombre = "Nico"
+empleado1.apellido = "Prusci"
+empleado1.telefono = 1178213792
+empleado1.setAsistenciaDia("none","AB",101010)#Abril lunes si  martes no  miercoles si  jueves no  viernes si  sabado no
+
+#INGRESANDO EMPLEADO A LA EMPRESA:::::::::::::::::::::::::::::
+Empresa.Ingresar_Empleado(empleado1)
+Empresa.existeEmpleado ("nico")    #devuelve un true o un false (Existe o no existe)
+print (Empresa.existeEmpleado ("nico"))
+
+#CONSULTAR DIAS DE INGRESO
+Empresa.diasTrabajados (AB)   #Dias que trabajo en abril
+print (Empresa.diasTrabajados (AB))
