d6845c11f9e32e468acd312473c3533af08c95ac
Sebastian Opderbeck <opderbeck.ipm@gmail.com>
2017-06-18 13:36:46 -0300

    18-06

--- TrabajoExpo/Juego.py
+++ TrabajoExpo/Juego.py
@@ -27,20 +27,18 @@ pygame.init()
 
 class Fondo(pygame.sprite.Sprite):
 
-
     def __init__(self):
         self.fondo = pygame.image.load("FondoPueblo.png").convert_alpha()
         self.rect = self.fondo.get_rect()
 
-    def update(self, pantalla, velocidadx, velocidady):
+    def update(self, screen, velocidadx, velocidady):
         self.rect.move_ip(-velocidadx, -velocidady)
-        pantalla.blit(self.fondo, self.rect)
-
+        screen.blit(self.fondo, self.rect)
 
 class Player(pygame.sprite.Sprite):
 
     def __init__(self):
-        # cargado de imagenes_______________________________________________________________________________________________
+        # cargado de imagenes___________________________________________________________________________________________
         self.PersonajeFrente = pygame.image.load("PersonajeFrente.png").convert_alpha()
         self.PersonajeFrenteCaminando1 = pygame.image.load("PersonajeFrenteCaminando1.png").convert_alpha()
         self.PersonajeFrenteCaminando2 = pygame.image.load("PersonajeFrenteCaminando2.png").convert_alpha()
@@ -54,79 +52,70 @@ class Player(pygame.sprite.Sprite):
         self.PersonajeIzquierdaCaminando1 = pygame.image.load("PersonajeIzquierdaCaminando1.png").convert_alpha()
         self.PersonajeIzquierdaCaminando2 = pygame.image.load("PersonajeIzquierdaCaminando2.png").convert_alpha()
 
-        # fondo = pygame.image.load("").convert_alpha()
+        self.listImages = [[self.PersonajeFrente, self.PersonajeFrenteCaminando1, self.PersonajeFrenteCaminando2],
+                              [self.PersonajeEspalda, self.PersonajeEspaldaCaminando1, self.PersonajeEspaldaCaminando2],
+                              [self.PersonajeDerecha, self.PersonajeDerechaCaminando1, self.PersonajeDerechaCaminando2],
+                              [self.PersonajeIzquierda, self.PersonajeIzquierdaCaminando1, self.PersonajeIzquierdaCaminando2]]
 
-        self.lista_imagenes = [[self.PersonajeFrente, self.PersonajeFrenteCaminando1, self.PersonajeFrenteCaminando2],
-                 [self.PersonajeEspalda, self.PersonajeEspaldaCaminando1, self.PersonajeEspaldaCaminando2],
-                 [self.PersonajeDerecha, self.PersonajeDerechaCaminando1, self.PersonajeDerechaCaminando2],
-                 [self.PersonajeIzquierda, self.PersonajeIzquierdaCaminando1, self.PersonajeIzquierdaCaminando2]]
-        self.imagen_actual = 0
-        self.imagen = self.lista_imagenes[0][self.imagen_actual]
+        self.currentImage = 0
+        self.image = self.listImages[0][self.currentImage]
 
-        self.rect = self.imagen.get_rect()
-        self.rect.top, self.rect.left = (100, 200)
+        self.rect = self.image.get_rect()
+        self.rect.top, self.rect.left = (50, 50)
 
-        self.movimiento = False
-        self.orientacion = 0
+        self.motion = False
+        self.orientation = 0
 
     def MovePJ(self, velocidadx, velocidady):
         self.rect.move_ip(velocidadx, velocidady)
 
-    def Update (self, screen, velocidadx, velocidady):
+    def Update(self, screen, velocidadx, velocidady):
         if (velocidadx, velocidady) == (0, 0):
-            self.movimiento = False
-            self.imagen_actual = 0
-        else: self.movimiento = True
-
+            self.motion = False
+            self.currentImage = 0
+        else:
+            self.motion = True
         if velocidady > 0:
-            self.orientacion = 0
-            if self.movimiento:
-                if self.imagen_actual == 0 or self.imagen_actual > 3: self.imagen_actual = 1
-                elif self.imagen_actual == 1: self.imagen_actual = 2
-                elif self.imagen_actual == 2: self.imagen_actual = 1
-
-        if velocidady < 0:
-            self.orientacion = 1
-            if self.movimiento:
-                if self.imagen_actual == 0 or self.imagen_actual > 3: self.imagen_actual = 1
-                elif self.imagen_actual == 1: self.imagen_actual = 2
-                elif self.imagen_actual == 2: self.imagen_actual = 1
-
+            self.orientation = 0
+            if self.motion:
+                if self.currentImage == 0 or self.currentImage > 3: self.currentImage = 1
+                elif self.currentImage == 1: self.currentImage = 2
+                elif self.currentImage == 2: self.currentImage = 1
+        elif velocidady < 0:
+            self.orientation = 1
+            if self.motion:
+                if self.currentImage == 0 or self.currentImage > 3: self.currentImage = 1
+                elif self.currentImage == 1: self.currentImage = 2
+                elif self.currentImage == 2: self.currentImage = 1
         if velocidadx > 0:
-            self.orientacion = 2
-            if self.movimiento:
-                if self.imagen_actual == 0 or self.imagen_actual > 3: self.imagen_actual = 1
-                elif self.imagen_actual == 1: self.imagen_actual = 2
-                elif self.imagen_actual == 2: self.imagen_actual = 1
-                if self.imagen_actual > 1: self.imagen_actual = 0
-
-        if velocidadx < 0:
-            self.orientacion = 3
-            if self.movimiento:
-                if self.imagen_actual == 0 or self.imagen_actual > 3: self.imagen_actual = 1
-                elif self.imagen_actual == 1: self.imagen_actual = 2
-                elif self.imagen_actual == 2: self.imagen_actual = 1
-                if self.imagen_actual > 1: self.imagen_actual = 0
+            self.orientation = 2
+            if self.motion:
+                if self.currentImage == 0 or self.currentImage > 3:self.currentImage = 1
+                elif self.currentImage == 1: self.currentImage = 2
+                elif self.currentImage == 2: self.currentImage = 1
+                if self.currentImage > 2: self.currentImage = 0
+        elif velocidadx < 0:
+            self.orientation = 3
+            if self.motion:
+                if self.currentImage == 0 or self.currentImage > 3: self.currentImage = 1
+                elif self.currentImage == 1: self.currentImage = 2
+                elif self.currentImage == 2: self.currentImage = 1
+                if self.currentImage > 2: self.currentImage = 0
 
         (velocidadx, velocidady) = (0, 0)
         self.MovePJ(velocidadx, velocidady)
-        self.imagen = self.lista_imagenes[self.orientacion][self.imagen_actual]
-        screen.blit(self.imagen, self.rect)
+        self.image = self.listImages[self.orientation][self.currentImage]
+        screen.blit(self.image, self.rect)
 
 
 def main():
-    # nombre de la ventana
     pygame.display.set_caption("JUEGUITO")
-
-    # tamaño de pantalla
     displaySize = (1000, 1000)
     screen = pygame.display.set_mode(displaySize)
 
-    # velocidad de refresco
     clock = pygame.time.Clock()
     fps = 20
 
-    # colores
     white = (255, 255, 255)
     black = (0, 0, 0)
     red = (255, 0, 0)
@@ -138,62 +127,49 @@ def main():
     fondo = Fondo()
     player = Player()
     velocidad = 10
-    vx, vy = 0, 0
+    (vx, vy) = (0, 0)
 
     left, right, up, down = False, False, False, False
-
 
     while finish != True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 finish = True
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                left = True
-                vx = -velocidad
-            if event.key == pygame.K_RIGHT:
-                vx = velocidad
-                right = True
-            if event.key == pygame.K_DOWN:
-                down = True
-                vy = velocidad
-            if event.key == pygame.K_UP:
-                up = True
-                vy = -velocidad
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                left = False
-                if right: vx = velocidad
-                else: vx = 0
-            if event.key == pygame.K_RIGHT:
-                right = False
-                if left: vx = -velocidad
-                else: vx = 0
-            if event.key == pygame.K_UP:
-                up = False
-                if down: vy = velocidad
-                else: vy = 0
-            if event.key == pygame.K_DOWN:
-                down = False
-                if up: vy = -velocidad
-                else: vy = 0
-
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    left = True
+                    vx = -velocidad
+                if event.key == pygame.K_RIGHT:
+                    vx = velocidad
+                    right = True
+                if event.key == pygame.K_DOWN:
+                    down = True
+                    vy = velocidad
+                if event.key == pygame.K_UP:
+                    up = True
+                    vy = -velocidad
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    left = False
+                    if right: vx = velocidad
+                    else: vx = 0
+                if event.key == pygame.K_RIGHT:
+                    right = False
+                    if left: vx = -velocidad
+                    else: vx = 0
+                if event.key == pygame.K_UP:
+                    up = False
+                    if down: vy = velocidad
+                    else: vy = 0
+                if event.key == pygame.K_DOWN:
+                    down = False
+                    if up: vy = -velocidad
+                    else: vy = 0
 
         clock.tick(fps)
         fondo.update(screen, vx, vy)
         player.update(screen, vx, vy)
         pygame.display.update()
-
-
-
-
-
-
-
-
-
 
 main()
 pygame.quit()
